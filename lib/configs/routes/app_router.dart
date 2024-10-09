cc9cd8083dba2c289634419d4b730993f0af57db
tuanpv62 <phantuan62.paster@gmail.com>
2024-10-09 22:26:10 +0700

    fix conflict merge from main

--- lib/configs/routes/app_router.dart
+++ lib/configs/routes/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movemate/features/booking/presentation/screens/service_screen/service_screen.dart';
 import 'package:movemate/features/profile/presentation/screens/contact/contact_screen.dart';
 import 'package:movemate/features/test_cloudinary/test_cloudinary_screen.dart';
 
