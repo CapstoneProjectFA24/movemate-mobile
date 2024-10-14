3470db753d962e78b45019f491aec5b854c58111
tuanpv62 <phantuan62.paster@gmail.com>
2024-10-14 11:49:14 +0700

    refactor(booking): clean up redundant code (#11.9)

--- lib/features/booking/presentation/widgets/booking_screen_2th/export_booking_screen_2th.dart
+++ lib/features/booking/presentation/widgets/booking_screen_2th/export_booking_screen_2th.dart
@@ -1,5 +1,4 @@
 export 'package:movemate/features/booking/presentation/widgets/booking_screen_2th/check_list_section.dart';
 export 'package:movemate/features/booking/presentation/widgets/booking_screen_2th/notes_section.dart';
-export 'package:movemate/features/booking/presentation/widgets/booking_screen_2th/package_selection.dart';
 export 'package:movemate/features/booking/presentation/widgets/booking_screen_2th/round_trip_checkbox.dart';
 export 'package:movemate/features/booking/presentation/widgets/booking_screen_2th/summary_section.dart';
