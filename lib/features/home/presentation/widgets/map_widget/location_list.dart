ab3150f1ea0ad56b11d2fc8df128a798376ae8a5
vinh <vinhntse2002@gmail.com>
2024-10-16 14:19:29 +0700

    refactor(map): refactor map sdk to app (#27)

---
+++ lib/features/home/presentation/widgets/map_widget/location_list.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:movemate/features/home/domain/entities/location_model_entities.dart';
+import 'package:movemate/utils/constants/asset_constant.dart';
+
+class LocationList extends StatelessWidget {
+  final List<LocationModel> locations;
+  final Function(LocationModel) onLocationSelected;
+
+  const LocationList({
+    Key? key,
+    required this.locations,
+    required this.onLocationSelected,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: locations.length,
+      itemBuilder: (context, index) {
+        final location = locations[index];
+        return ListTile(
+          leading:
+              const Icon(Icons.location_on, color: AssetsConstants.primaryDark),
+          title: Text(
+            location.label,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+              color: AssetsConstants.blackColor,
+            ),
+          ),
+          subtitle: Text(
+            location.address,
+            style: const TextStyle(
+              fontSize: 12,
+              color: AssetsConstants.greyColor,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          trailing: Text(
+            location.distance,
+            style: const TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          onTap: () => onLocationSelected(location),
+        );
+      },
+    );
+  }
+}
