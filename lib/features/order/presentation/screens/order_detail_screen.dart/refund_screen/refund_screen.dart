4d68e57081dc777f6519f92bb2960fcdcb162f26
tuanpv62 <phantuan62.paster@gmail.com>
2024-12-08 18:54:08 +0700

    refactor (redund) add page refund page (#178)

--- lib/features/order/presentation/screens/order_detail_screen.dart/refund_screen/refund_screen.dart
+++ lib/features/order/presentation/screens/order_detail_screen.dart/refund_screen/refund_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
+import 'package:movemate/configs/routes/app_router.dart';
 import 'package:movemate/features/booking/data/models/resquest/cancel_booking.dart';
 import 'package:movemate/features/booking/presentation/screens/controller/booking_controller.dart';
 import 'package:movemate/features/booking/presentation/widgets/booking_screen_2th/booking_package/sub_service_tile.dart';
@@ -21,11 +22,8 @@ class RefundScreen extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final currencyFormatter = NumberFormat.currency(
-      locale: 'vi_VN',
-      symbol: 'VND',
-      decimalDigits: 0,
-    );
+    final state = ref.read(bookingControllerProvider);
+
     final reasons = [
       'Bất khả kháng',
       'Muốn thay đổi dịch vụ trong đơn hàng',
@@ -35,142 +33,162 @@ class RefundScreen extends HookConsumerWidget {
     ];
 
     final selectedReason = useState<String?>(null);
-    return Scaffold(
-      backgroundColor: const Color(0xFFF5F5F5),
-      appBar: const CustomAppBar(
-        backgroundColor: AssetsConstants.primaryMain,
-        backButtonColor: AssetsConstants.whiteColor,
-        title: "Yêu cầu hoàn tiền",
-        centerTitle: true,
-        // iconSecond: Icons.home_outlined,
-        // onCallBackSecond: () {
-        //   final tabsRouter = context.router.root
-        //       .innerRouterOf<TabsRouter>(TabViewScreenRoute.name);
-        //   if (tabsRouter != null) {
-        //     tabsRouter.setActiveIndex(0);
-        //     context.router.popUntilRouteWithName(TabViewScreenRoute.name);
-        //   } else {
-        //     context.router.pushAndPopUntil(
-        //       const TabViewScreenRoute(children: [
-        //         HomeScreenRoute(),
-        //       ]),
-        //       predicate: (route) => false,
-        //     );
-        //   }
-        // },
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'Lý do hoàn lại',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 10),
-              ...reasons.map((reason) => RadioListTile(
-                    title: Text(
-                      reason,
-                      style: const TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                    value: reason,
-                    activeColor: Colors.orangeAccent,
-                    groupValue: selectedReason.value,
-                    onChanged: (value) {
-                      selectedReason.value = value;
-                      // Xử lý khi chọn lý do
-                      print('Đã chọn lý do: $value');
-                    },
-                  )),
-              const SizedBox(height: 10),
-              const Text(
-                'Chính sách hoàn lại khác nhau dựa trên thời gian hủy đơn dọn nhà của bạn',
-                style: TextStyle(fontSize: 16, color: Color(0xFF666666)),
-              ),
-              const SizedBox(height: 20),
-              _buildRefundPolicyCard(
-                title: 'Giá trị hoàn ước tính',
-                content:
-                    'Hoàn tiền 100% tiền đặt cọc nếu hủy đặt lịch ít nhất 48 giờ trước thời gian dọn nhà',
-                amount: formatPrice(order.deposit.toInt()),
-              ),
-              const SizedBox(height: 10),
-              _buildRefundPolicyCard(
-                title: 'Hoàn Tiền 70% tiền đặt cọc',
-                content:
-                    'Nếu hủy dịch vụ trong vòng 24 đến 48 giờ trước thời gian dọn nhà, MoveMate sẽ hoàn lại 70% phí đặt lịch để bù đắp chi phí chuẩn bị',
-                amount: formatPrice((order.deposit * 0.7).toInt()),
-              ),
-              const SizedBox(height: 10),
-              _buildRefundPolicyCard(
-                title: 'Hoàn Tiền 50% tiền đặt cọc',
-                content:
-                    'Nếu hủy trong vòng 1 đến 24 giờ trước thời gian dọn nhà, MoveMate sẽ hoàn lại 50% phí dịch vụ',
-                amount: formatPrice((order.deposit * 0.5).toInt()),
-              ),
-              const SizedBox(height: 20),
-              const SizedBox(height: 20),
-              _buildFAQItem(
-                'Quá trình hoàn tiền mất bao lâu?',
-                'Quy trình hoàn tiền phụ thuộc vào chính sách của nhà cung cấp dịch vụ. MoveMate sẽ cập nhật thông tin đến quý khách trong suốt quá trình xử lý',
-              ),
-              const SizedBox(height: 20),
-              _buildFAQItem(
-                'Làm thế nào tôi có thể gửi yêu cầu hoàn tiền?',
-                'Nhấp Bắt đầu Hoàn tiền bên dưới và chúng tôi sẽ xử lý yêu cầu của bạn. Đừng lo, đơn hàng của bạn vẫn còn hiệu lực cho đến khi bạn gửi yêu cầu hoàn tiền ở bên dưới',
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                width: double.infinity,
-                height: 54,
-                child: ElevatedButton(
-                  onPressed: (selectedReason.value != null)
-                      ? () async {
-                          final cancelRequest = CancelBooking(
-                            id: order.id,
-                            cancelReason: selectedReason.value ?? '',
-                          );
-
-                          await ref
-                              .read(bookingControllerProvider.notifier)
-                              .cancelBooking(
-                                request: cancelRequest,
-                                id: order.id,
-                                context: context,
-                              );
-                        }
-                      : () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.transparent,
-                    elevation: 0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      side: BorderSide(
-                        color: (selectedReason.value != null)
-                            ? Colors.orange
-                            : Colors.grey,
-                        width: 1,
-                      ),
-                    ),
-                  ),
-                  child: LabelText(
-                    content: 'Yêu cầu hoàn tiền',
-                    size: 14,
-                    color: (selectedReason.value != null)
-                        ? Colors.orange
-                        : Colors.grey,
+    return LoadingOverlay(
+      isLoading: state.isLoading,
+      child: Scaffold(
+        backgroundColor: const Color(0xFFF5F5F5),
+        appBar: const CustomAppBar(
+          backgroundColor: AssetsConstants.primaryMain,
+          backButtonColor: AssetsConstants.whiteColor,
+          title: "Yêu cầu hoàn tiền",
+          centerTitle: true,
+          // iconSecond: Icons.home_outlined,
+          // onCallBackSecond: () {
+          //   final tabsRouter = context.router.root
+          //       .innerRouterOf<TabsRouter>(TabViewScreenRoute.name);
+          //   if (tabsRouter != null) {
+          //     tabsRouter.setActiveIndex(0);
+          //     context.router.popUntilRouteWithName(TabViewScreenRoute.name);
+          //   } else {
+          //     context.router.pushAndPopUntil(
+          //       const TabViewScreenRoute(children: [
+          //         HomeScreenRoute(),
+          //       ]),
+          //       predicate: (route) => false,
+          //     );
+          //   }
+          // },
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Lý do hoàn lại',
+                  style: TextStyle(
+                    fontSize: 18,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 10),
+                ...reasons.map((reason) => RadioListTile(
+                      title: Text(
+                        reason,
+                        style: const TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      value: reason,
+                      activeColor: Colors.orangeAccent,
+                      groupValue: selectedReason.value,
+                      onChanged: (value) {
+                        selectedReason.value = value;
+                        // Xử lý khi chọn lý do
+                        print('Đã chọn lý do: $value');
+                      },
+                    )),
+                const SizedBox(height: 10),
+                const Text(
+                  'Chính sách hoàn lại khác nhau dựa trên thời gian hủy đơn dọn nhà của bạn',
+                  style: TextStyle(fontSize: 16, color: Color(0xFF666666)),
+                ),
+                const SizedBox(height: 20),
+                _buildRefundPolicyCard(
+                  title: 'Giá trị hoàn ước tính',
+                  content:
+                      'Hoàn tiền 100% tiền đặt cọc nếu hủy đặt lịch ít nhất 48 giờ trước thời gian dọn nhà',
+                  amount: formatPrice(order.deposit.toInt()),
+                ),
+                const SizedBox(height: 10),
+                _buildRefundPolicyCard(
+                  title: 'Hoàn Tiền 70% tiền đặt cọc',
+                  content:
+                      'Nếu hủy dịch vụ trong vòng 24 đến 48 giờ trước thời gian dọn nhà, MoveMate sẽ hoàn lại 70% phí đặt lịch để bù đắp chi phí chuẩn bị',
+                  amount: formatPrice((order.deposit * 0.7).toInt()),
+                ),
+                const SizedBox(height: 10),
+                _buildRefundPolicyCard(
+                  title: 'Hoàn Tiền 50% tiền đặt cọc',
+                  content:
+                      'Nếu hủy trong vòng 1 đến 24 giờ trước thời gian dọn nhà, MoveMate sẽ hoàn lại 50% phí dịch vụ',
+                  amount: formatPrice((order.deposit * 0.5).toInt()),
+                ),
+                const SizedBox(height: 20),
+                const SizedBox(height: 20),
+                _buildFAQItem(
+                  'Quá trình hoàn tiền mất bao lâu?',
+                  'Quy trình hoàn tiền phụ thuộc vào chính sách của nhà cung cấp dịch vụ. MoveMate sẽ cập nhật thông tin đến quý khách trong suốt quá trình xử lý',
+                ),
+                const SizedBox(height: 20),
+                _buildFAQItem(
+                  'Làm thế nào tôi có thể gửi yêu cầu hoàn tiền?',
+                  'Nhấp Bắt đầu Hoàn tiền bên dưới và chúng tôi sẽ xử lý yêu cầu của bạn. Đừng lo, đơn hàng của bạn vẫn còn hiệu lực cho đến khi bạn gửi yêu cầu hoàn tiền ở bên dưới',
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: double.infinity,
+                  height: 54,
+                  child: ElevatedButton(
+                    onPressed: (selectedReason.value != null || state.isLoading)
+                        ? () async {
+                            final tabsRouter = context.router.root
+                                .innerRouterOf<TabsRouter>(
+                                    TabViewScreenRoute.name);
+                            final cancelRequest = CancelBooking(
+                              id: order.id,
+                              cancelReason: selectedReason.value ?? '',
+                            );
+
+                            await ref
+                                .read(bookingControllerProvider.notifier)
+                                .cancelBooking(
+                                  request: cancelRequest,
+                                  id: order.id,
+                                  context: context,
+                                );
+
+                            if (tabsRouter != null) {
+                              tabsRouter.setActiveIndex(0);
+                              context.router.popUntilRouteWithName(
+                                  TabViewScreenRoute.name);
+                            } else {
+                              context.router.pushAndPopUntil(
+                                const TabViewScreenRoute(children: [
+                                  HomeScreenRoute(),
+                                ]),
+                                predicate: (route) => false,
+                              );
+                            }
+                          }
+                        : null,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        side: BorderSide(
+                          color:
+                              (selectedReason.value != null || state.isLoading)
+                                  ? Colors.orange
+                                  : Colors.grey,
+                          width: 1,
+                        ),
+                      ),
+                    ),
+                    child: LabelText(
+                      content: 'Yêu cầu hoàn tiền',
+                      size: 14,
+                      color: (selectedReason.value != null || state.isLoading)
+                          ? Colors.orange
+                          : Colors.grey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
